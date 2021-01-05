4f2da51cc4f05f6466d5bf92bfa69ff5e97a0a8b
Rob <robert5guerra@gmail.com>
2021-01-04 19:42:50 -0800

    added load_menu() to main and added play text

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from models.PlayerModel import Player
 from models.BoardModel import Board
 from helper import *
-import menu
+from menu import load_menu
 
 
 # START
@@ -10,13 +10,13 @@ print('{}'.format('\n') * 10)
 print("\n\n\n\t\tWelcome!\n------------------------\n")
 restart = 'y'
 
-menu.load_menu()
 
 # Start/Restart Game
 while restart.lower() == 'y':
     # Player/Board Instances
     player1 = Player(player_number=1)
     player2 = Player(player_number=2)
+    load_menu()
     board = Board()
 
     icon_dict = {k: '' for k in [i for i in range(1, 10)]}
--- menu.py
+++ menu.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 
 
 def load_menu():
@@ -30,11 +29,12 @@ def load_menu():
     height = screen.get_height()
 
     # defining a font
-    smallfont = pygame.font.SysFont('Corbel', 35)
+    small_font = pygame.font.SysFont('Corbel', 35)
 
     # rendering a text written in
     # this font
-    text = smallfont.render('quit', True, color)
+    play = small_font.render('play', True, color)
+    text = small_font.render('quit', True, color)
 
     while True:
 
@@ -61,14 +61,14 @@ def load_menu():
         # if mouse is hovered on a button it
         # changes to lighter shade
         if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
-            pygame.draw.rect(screen, color_light, [width / 2, height / 2, 140, 40])
+            pygame.draw.rect(screen, color_light, [width / 4 + 100, height / 2, 140, 40])
 
         else:
-            pygame.draw.rect(screen, color_dark, [width / 2, height / 2, 140, 40])
+            pygame.draw.rect(screen, color_dark, [width / 4 + 100, height / 2, 140, 40])
 
             # superimposing the text onto our button
-        screen.blit(text, (width / 2 + 50, height / 2))
+        screen.blit(play, (width / 4 + 100, height / 4 + 100))
+        screen.blit(text, (width / 4 + 100, height / 2))
 
         # updates the frames of the game
         pygame.display.update()
-
